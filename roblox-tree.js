22a1cf009bb9ab62403e7700b33fe89e7747be2f
David Minnerly <dtreeLive@hotmail.com>
2017-01-17 12:35:28 -0500

    Refactor how we split filename parts

--- roblox-tree.js
+++ roblox-tree.js
@@ -9,14 +9,27 @@ function isDirectory(filePath) {
   return fs.statSync(filePath).isDirectory();
 }
 
-function getBaseNameParts(filePath) {
-  const base = path.basename(filePath);
-  const parts =  base.split('.');
+// Splits the filename into named parts.
+//
+// We don't have access to the Roblox Studio Properties panel, so we have to add
+// on ClassName identifiers to the filenames.
+//
+// Our files will typically look like 'Filename.class.lua', where 'class' can be
+// 'module', 'local' or 'script'. These are used as ClassName identifiers so
+// that we can later turn them into Roblox's supported ClassNames.
+//
+// Example:
+//
+//  splitBasename('Helpers.module.lua')
+//  { name: 'Helpers', classIdentifier: 'module', ext: '.lua' }
+function splitBasename(filePath) {
+  const parsed = path.parse(filePath);
+  const parts = parsed.name.split('.');
 
   return {
     name: parts[0],
-    className: parts[1],
-    extension: parts[2]
+    classIdentifier: parts[1],
+    ext: parsed.ext
   };
 }
 
@@ -42,11 +55,11 @@ function getScriptClassFromWord(word) {
 }
 
 function luaFileToRobloxObject(filePath) {
-  const parts = getBaseNameParts(filePath);
+  const parts = splitBasename(filePath);
 
   return {
     name: parts.name,
-    className: getScriptClassFromWord(parts.className),
+    className: getScriptClassFromWord(parts.classIdentifier),
     source: getFileContents(filePath)
   };
 }
