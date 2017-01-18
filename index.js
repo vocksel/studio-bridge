d533d70cb354c4424e0741d9020a620fbbc24637
David Minnerly <dtreeLive@hotmail.com>
2017-01-17 19:44:17 -0500

    Move modules to their own directory

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const process = require('process');
 
-const server = require('./server.js');
+const server = require('./lib/server.js');
 
 const projectDir = process.argv[2] && path.resolve(__dirname, process.argv[2]) || __dirname;
 const port = process.argv[3] || 8080;
