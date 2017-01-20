0a35dec68511d48c97a2a9b422a2f063cfdc8700
David Minnerly <dtreeLive@hotmail.com>
2017-01-20 15:17:17 -0500

    Initialize settings when the plugin first runs

--- plugin/src/StudioBridge/Main.lua
+++ plugin/src/StudioBridge/Main.lua
@@ -1,8 +1,33 @@
 local PLUGIN_NAME = "Studio Bridge"
 
+--------------------------------------------------------------------------------
+-- Plugin Setup
+--------------------------------------------------------------------------------
+
 -- Suppresses the "unknown global" warnings.
 local plugin = plugin
 local toolbar = plugin:CreateToolbar(PLUGIN_NAME)
+
+--------------------------------------------------------------------------------
+-- Settings
+--------------------------------------------------------------------------------
+
+local function initializeSettings()
+  local runBefore = plugin:GetSetting("RunBefore") or false
+
+  if not runBefore then
+    plugin:SetSettings("RunBefore", true)
+
+    plugin:SetSettings("AutoSync", false)
+    plugin:SetSettings("RefreshRate", .25)
+  end
+end
+
+initializeSettings()
+
+--------------------------------------------------------------------------------
+-- Button Setup
+--------------------------------------------------------------------------------
 
 local function createSyncButton()
   local tooltip = "Establishes a connection to the server and starts syncing "..
