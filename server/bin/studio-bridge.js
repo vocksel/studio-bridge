fb199ceb7f8e8f71528d6a8140513b11e54e7bc1
David Minnerly <dtreeLive@hotmail.com>
2017-01-23 14:03:00 -0500

    Use commander.js to create a CLI for the server

---
+++ server/bin/studio-bridge.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+
+const path = require('path');
+const fs = require('fs');
+
+const program = require('commander');
+
+const pkg = require('../package.json');
+const server = require('../lib/server.js');
+
+function failWithHelp(msg) {
+  console.log(msg);
+  program.help();
+  process.exit(1);
+}
+
+program
+  .version(pkg.version)
+  .arguments('<dir>')
+  .option('-p, --port <port>', 'the port to run the server off of. defaults to 8080.', 8080)
+  .action(dir => {
+    const fullPath = path.resolve(dir);
+
+    if (fs.existsSync(fullPath)) {
+      server(fullPath, program.port);
+    } else {
+      failWithHelp(`Could not find a directory at ${fullPath}`)
+    }
+  });
+
+program.parse(process.argv);
+
+if (!program.args[0])
+  failWithHelp('The directory argument is required.');
